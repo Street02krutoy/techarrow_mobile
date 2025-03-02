a1f424f1f4a97953fcf81b081b5f3feed1733e73
Gleb <gabramov723@gmail.com>
2025-03-02 10:51:46 +0300

    попытка в коллажи 2

--- lib/screens/draw/collageCreator.dart
+++ lib/screens/draw/collageCreator.dart
@@ -1,56 +1,40 @@
-import 'dart:ui';
-
-import 'package:image/image.dart' as img;
+import 'package:image/image.dart';
 
 class CollageCreator {
-  int rowNumber = 2;
-  int colNumber = 2;
+  int rows = 2;
+  int cols = 2;
   int resultWidth = 1080;
   int resultHeight = 1920;
-  Color gridColor = const Color(0xFF000000);
-  List<img.Image> images;
+  //Color gridColor = const Color(0xFF000000);
+  List<Image> images;
 
   CollageCreator(this.images, {int row = 2, int col = 2}) {
-    rowNumber = row;
-    colNumber = col;
-  }
-
-  img.Image createCollage({int resultWidth = 1080, int resultHeight = 1920}) {
-
-    // Определяем размеры каждого изображения
-    int width = resultWidth ~/ colNumber;
-    int height = resultHeight ~/ rowNumber;
-
-    if (rowNumber < 1 || colNumber < 1) {
-      throw Exception('Неверное количество рядов и колонок');
-    }
-    while (images.length < rowNumber * colNumber) {
-      images.add(img.Image.new(width, height));
+      rows = row;
+      cols = col;
     }
 
-    // Создаем изображение для коллажа
-    img.Image collage = img.Image(resultWidth, resultHeight);
+    Image createCollage() {
+    while (images.length < rows * cols){
+      images.add(Image(540, 960));
+      print('added empty image');
+    }
 
-    // Отрисовываем изображения на коллаже
-    for (int i = 0; i < rowNumber; i++) {
-      for (int j = 0; j < colNumber; j++) {
-        int index = i * colNumber + j;
-        if (index < images.length) {
-          img.Image resizedImage =
-              img.copyResize(images[index], width: width, height: height);
-          img.copyInto(collage, resizedImage,
-              dstX: j * width, dstY: i * height);
-        }
+    // Определим размеры ячейки
+    int cellWidth = images.first.width;
+    int cellHeight = images.first.height;
+
+    // Создадим итоговое изображение
+    int totalWidth = cellWidth * cols;
+    int totalHeight = cellHeight * rows;
+    Image collage = Image(totalWidth, totalHeight);
+
+    // Разместим изображения в коллаже
+    for (int i = 0; i < rows; i++) {
+      for (int j = 0; j < cols; j++) {
+        int index = i * cols + j;
+        Image image = images[index];
+        copyInto(collage, image, dstX: j * cellWidth, dstY: i * cellHeight);
       }
-    }
-
-    for (int i = 0; i <= rowNumber; i++) {
-      img.drawLine(
-          collage, 0, i * height, resultWidth, i * height, gridColor.value);
-    }
-    for (int j = 0; j <= colNumber; j++) {
-      img.drawLine(
-          collage, j * width, 0, j * width, resultHeight, gridColor.value);
     }
 
     return collage;
